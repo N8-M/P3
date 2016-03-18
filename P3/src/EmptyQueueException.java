81f1463bdbdb081c5df4b435ba5d5f04496d90a0
N8-M <nmindham@wisc.edu>
2016-03-18 16:22:28 -0500

    Update EmptyQueueException.java

--- P3/src/EmptyQueueException.java
+++ P3/src/EmptyQueueException.java
@@ -1,3 +1,41 @@
+///////////////////////////////////////////////////////////////////////////////
+//                   ALL STUDENTS COMPLETE THESE SECTIONS
+// Main Class File:  EmptyQueueException.java
+// File:             Course.java
+// Semester:         CS 367 Spring 2016
+//
+// Author:           Neight Mindham
+// Email:            Nmindham@wisc.edu
+// CS Login:         Neight
+// Lecturer's Name:  Deb Deppeler
+//
+//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ////////////////////
+
+// Pair Partner:     Luke Van Hulle
+// Email:            lvanhulle@wisc.edu
+// CS Login:         van-hulle
+// Lecturer's Name:  Deb Deppeler	
+////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//                   ALL STUDENTS COMPLETE THESE SECTIONS
+// Main Class File:  StudentCenter.java
+// File:             EmptyQueueException.java
+// Semester:         CS 367 Spring 2016
+//
+// Author:           Luke Van Hulle
+// Email:            lvanhulle@wisc.edu
+// CS Login:         van-hulle
+// Lecturer's Name:  Deb Deppeler
+//
+//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ////////////////////
+ 
+// Pair Partner:     Neight Mindham
+// Email:            nmindham@wisc.edu
+// CS Login:         Neight
+// Lecturer's Name:  Deb Deppeler	
+////////////////////////////////////////////////////////////////////////////////
+
 /**
  * Thrown when trying to access an item in an empty queue.
  */
