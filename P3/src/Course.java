443f0e1e5720a7be2d474e38794770199d7795f4
N8-M <nmindham@wisc.edu>
2016-03-20 13:34:03 -0500

    Update Course.java
    Added in method headers, class headers, and comments of code. Should be done up to where we are in coding.

--- P3/src/Course.java
+++ P3/src/Course.java
@@ -1,3 +1,43 @@
+///////////////////////////////////////////////////////////////////////////////
+//                   ALL STUDENTS COMPLETE THESE SECTIONS
+// Main Class File:  StudentCenter.java
+// File:             Course.java
+// Semester:         CS 367 Spring 2016
+//
+// Author:           Neight Mindham
+// CS Login:         Neight
+// Lecturer's Name:  Deb Deppeler
+// 
+//
+//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ////////////////////
+//
+// Pair Partner:     Luke Van Hulle
+// Email:            lvanHulle@wisc.edu
+// CS Login:         van-hulle
+// Lecturer's Name:  Deb Deppeler
+//
+////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//                   ALL STUDENTS COMPLETE THESE SECTIONS
+// Main Class File:  StudentCenter.java
+// File:             Course.java
+// Semester:         CS 367 Spring 2016
+//
+// Author:           Luke Van Hulle
+// CS Login:         Van-hulle
+// Lecturer's Name:  Deb Deppeler
+// 
+//
+//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ////////////////////
+//
+// Pair Partner:     Neight Mindham
+// Email:            Nmindham@wisc.edu
+// CS Login:         Neight
+// Lecturer's Name:  Deb Deppeler
+//
+////////////////////////////////////////////////////////////////////////////////
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +50,7 @@ import java.util.List;
 
 public class Course
 {
-
+	// states field variables
 	private String courseCode;
 	private String name;
 
@@ -24,10 +64,22 @@ public class Course
 
 	// List of students who are finally enrolled in the course
 	private List<Student> courseRoster;
-
+	
+	/**
+	 * Initializes the fields with the parameters sent in. 
+ 	* 
+ 	* 
+ 	*
+	* @param classCode: string that tells the class's code 
+ 	* @param name: Name of the student
+ 	* @param maxCapacity: max Capacity for the class
+ 	* 
+ 	*/
 	public Course(String classCode, String name, int maxCapacity)
 	{
 		// TODO initialize all parameters
+		
+		// initializes parameters
 		courseCode = classCode;
 		this.name = name; 
 		this.maxCapacity = maxCapacity; 
@@ -55,9 +107,14 @@ public class Course
 		// is done in the enqueue method.
 
 		// TODO : see function header
-
+		
+		// variable to hold the new student with priority coins
 		PriorityQueueItem<Student> temp = new PriorityQueueItem<Student>(coins);
+		
+		// adds student 
 		temp.add(student);
+		
+		// adds the student to the priority queue
 		registrationQueue.enqueue(temp);
 
 	}
@@ -70,27 +127,52 @@ public class Course
 	{
 		// TODO : populate courseRoster from registrationQueue
 		// Use the PriorityQueueIterator for this task.
+		
+		// creates a new iterator over registration queue
 		PriorityQueueIterator<Student> itr = new PriorityQueueIterator<Student>(registrationQueue); 
+		
+		// while loop to 
 		while(courseRoster.size() < maxCapacity && itr.hasNext()){
 
 		}
 	}
-
+	
+	/**
+ 	* Gets the course name
+ 	* 
+	* @return courseName: name of the course
+	*/
 	public String getCourseName()
 	{
 		// TODO
-		return null;
+		
+		// returns the coursename
+		return courseName;
 	}
 
+	/**
+ 	* Gets the courseCode
+ 	* 
+	* @return courseCode: code for the course
+	*/
 	public String getCourseCode()
 	{
 		// TODO
+		
+		// returns the coursecode
 		return null;
 	}
 
+	/**
+ 	* Gets the courseRegister
+ 	* 
+	* @return courseRegister: list of students in the courseRegister
+	*/
 	public List<Student> getCourseRegister()
 	{
 		// TODO
+		
+		// returns the list of sturdents for course register
 		return null;
 	}
 }
