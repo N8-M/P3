c0f513ac7c694d03f0bce6280171e3faeadbee96
N8-M <nmindham@wisc.edu>
2016-03-20 13:55:53 -0500

    Update PriorityQueue.java
    Should be done for all comments.

--- P3/src/PriorityQueue.java
+++ P3/src/PriorityQueue.java
@@ -1,3 +1,43 @@
+///////////////////////////////////////////////////////////////////////////////
+//                   ALL STUDENTS COMPLETE THESE SECTIONS
+// Main Class File:  StudentCenter.java
+// File:             PriorityQueue.java
+// Semester:         CS 367 Spring 2016
+//
+// Author:           Neight Mindham
+// CS Login:         Neight
+// Lecturer's Name:  Deb Deppeler
+// 
+//
+//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ////////////////////
+//
+// Pair Partner:     Luke Van Hulle
+// Email:            lvanHulle@wisc.edu
+// CS Login:         van-hulle
+// Lecturer's Name:  Deb Deppeler
+//
+////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+//                   ALL STUDENTS COMPLETE THESE SECTIONS
+// Main Class File:  StudentCenter.java
+// File:             PriorityQueue.java
+// Semester:         CS 367 Spring 2016
+//
+// Author:           Luke Van Hulle
+// CS Login:         Van-hulle
+// Lecturer's Name:  Deb Deppeler
+// 
+//
+//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ////////////////////
+//
+// Pair Partner:     Neight Mindham
+// Email:            Nmindham@wisc.edu
+// CS Login:         Neight
+// Lecturer's Name:  Deb Deppeler
+//
+////////////////////////////////////////////////////////////////////////////////
+
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -63,22 +103,42 @@ public class PriorityQueue<E> implements QueueADT<PriorityQueueItem<E>>
 
 
 		// Check all nodes and find if one with equal priority exists.
+		
+		// boolean to find out if found equal priority
 		boolean found = false;
+		
+		// for loop to check for item
 		for(int i = 1; i < currentSize && !found; i++){
+			
+			// checks if equal priority
 			if(array[i].getPriority() == item.getPriority()){
+				
+				// if found changes to true
 				found = true;
+				
+				// queue to hold items list
 				Queue<E> queue = item.getList();
+				
+				// adds the items in queue to array
 				while(queue.size() > 0){
 					array[i].add(queue.dequeue());
 				}
 			}				
 		}
+		
+		// if not found create new node 
 		if(!found){
+			// increments current size
 			currentSize++;
+			
+			// checks to see if we need to double size
 			if(currentSize+1 == array.length){
 				doubleArray(); 
 			}			
+			// adds to the last index
 			array[currentSize] = item;
+			
+			//perculates the item into the right location in PQ
 			perculateUp(currentSize);
 
 		}
@@ -97,6 +157,7 @@ public class PriorityQueue<E> implements QueueADT<PriorityQueueItem<E>>
 	public int size()
 	{
 		// TODO write appropriate code
+		// returns currentSize
 		return currentSize;
 	}
 
@@ -123,6 +184,7 @@ public class PriorityQueue<E> implements QueueADT<PriorityQueueItem<E>>
 	public PriorityQueueItem<E> peek()
 	{
 		// TODO fill in appropriate code, replace default return statement
+		// looks at first item in queue
 		return array[1];
 	}
 
@@ -140,7 +202,11 @@ public class PriorityQueue<E> implements QueueADT<PriorityQueueItem<E>>
 		// TODO
 		// Remove first element
 		PriorityQueueItem<E> temp = array[1];
+		
+		// takes last item in moves it to the first item
 		array[1] = array[currentSize--];
+		
+		// adjusts PQ
 		percolateDown(1);
 
 		// Replace with last element, percolate down
@@ -164,7 +230,11 @@ public class PriorityQueue<E> implements QueueADT<PriorityQueueItem<E>>
 	public void clear()
 	{
 		// TODO write appropriate code
+		
+		// creates a new PQ and points array to it
 		this.array = (PriorityQueueItem<E>[]) Array.newInstance(PriorityQueueItem.class, DEFAULT_CAPACITY);
+		
+		// resets current size
 		currentSize = 0; 
 	}
 
@@ -178,16 +248,24 @@ public class PriorityQueue<E> implements QueueADT<PriorityQueueItem<E>>
 	private void percolateDown(int hole)
 	{
 		// TODO
+		
+		// the left child
 		PriorityQueueItem<E> left = array[hole*2];
+		
+		// right child
 		PriorityQueueItem<E> right = array[(hole*2)+1];
+		
+		// parent
 		PriorityQueueItem<E> parent = array[hole];
-
+		
+		// checks if parent is smaller than right child. if so, swaps them
 		if(parent.compareTo(right) == -1){
 			PriorityQueueItem<E> temp = parent;
 			array[hole] = array[(hole*2)+1];
 			array[(hole*2)+1] = temp;
 			percolateDown((hole*2)+1);
 		}
+		// checks if parent is smaller than left child. if so, swaps them
 		else if(parent.compareTo(left) == -1){
 			PriorityQueueItem<E> temp = parent;
 			array[hole] = array[(hole*2)];
@@ -196,11 +274,22 @@ public class PriorityQueue<E> implements QueueADT<PriorityQueueItem<E>>
 		}
 	}
 
+	/**
+	* Perculates up the child in the array
+ 	* 
+ 	*
+ 	* @param child: variable to be moved up if needed
+ 	*/
+ 	
 	private void perculateUp(int child) {
 		// TODO Auto-generated method stub
+		// child
 		PriorityQueueItem<E> childItem = array[child];
+		
+		// parent of child
 		PriorityQueueItem<E> parent = array[child/2];
-
+		
+		// checks to see if child is greater than parent. if so, swap them
 		if(childItem.compareTo(parent) == 1){
 			PriorityQueueItem<E> temp = parent;
 			array[child/2] = array[child];
@@ -214,21 +303,23 @@ public class PriorityQueue<E> implements QueueADT<PriorityQueueItem<E>>
 	 * Internal method to expand array.
 	 */
 	private void doubleArray()
-	{
+	{	
+		// creates new array twice as big
 		PriorityQueueItem<E>[] newArray;
-
 		newArray = (PriorityQueueItem<E>[]) Array.newInstance(PriorityQueueItem.class, array.length * 2);
-
+		
+		// copies into new array
 		for (int i = 0;i < array.length;i++)
 			newArray[i] = array[i];
+		
+		// assigns to the new array	
 		array = newArray;
 	}
 
 	@Override
 	public boolean isEmpty()
 	{
-		if(currentSize == 0)
-			return true;
-		return false;
+		// returns truth value of currentsize == 0
+		return currentSize == 0;
 	}
 }
